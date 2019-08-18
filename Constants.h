6f3dc71b5087335db0884c7c2dbdf63036b126a9
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-08-18 01:58:38 -0700

    Add new global variables to constants file

--- Constants.h
+++ Constants.h
@@ -2,8 +2,31 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-//window variables
+//window global variables
+const SDL_Color BG_COLOR = { 0, 0, 0, 255 }; //black
 const int SCREEN_WIDTH = 960;
 const int SCREEN_HEIGHT = 640;
+const int MS_PER_FRAME = 16;
+
+//survivor global variables
+const int SURVIVOR_STARTING_X = 0;
+const int SURVIVOR_STARTING_Y = 0;
+const int SURVIVOR_STARTING_DIRECTION = 2;
+
+const int SURVIVOR_WIDTH = 48;
+const int SURVIVOR_HEIGHT = 48;
+
+/*VIEWPORTS*/
+//Info viewport
+const SDL_Color INFO_COLOR = { 47, 79, 79, 255 }; //dark slate gray
+const SDL_Rect INFO_VIEWPORT = { 10, 10, 940, 70 }; //x, y, width, height
+const SDL_Rect INFO = { 0, 0, 940, 70 };
+
+//Game viewport
+const SDL_Color GAME_COLOR = { 105, 105, 105, 255 }; //dim gray
+const SDL_Rect GAME_VIEWPORT = { 10, 90, 940, 540 }; //x, y, width, height
+const SDL_Rect GAME = { 0, 0, 940, 540 };
+const int GAME_WIDTH = 940;
+const int GAME_HEIGHT = 540;
 
 #endif
