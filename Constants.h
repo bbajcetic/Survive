73ab2be59a56a8500f8183d39b4c504b52f54fb3
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-08-18 13:48:56 -0700

    Change Game and Info area
    Game area is now 960x576 pixels, which is equivalent to a 20x12 grid
    size with each square being 48x48 pixels

--- Constants.h
+++ Constants.h
@@ -4,8 +4,8 @@
 
 //window global variables
 const SDL_Color BG_COLOR = { 0, 0, 0, 255 }; //black
-const int SCREEN_WIDTH = 960;
-const int SCREEN_HEIGHT = 640;
+const int SCREEN_WIDTH = 1190;
+const int SCREEN_HEIGHT = 596;
 const int MS_PER_FRAME = 16;
 
 //survivor global variables
@@ -15,21 +15,21 @@ const int SURVIVOR_STARTING_ANGLE = 90;
 const int SURVIVOR_STARTING_SPEED = 5;
 const int SURVIVOR_STARTING_DIRECTION = 1;
 
-const int SURVIVOR_WIDTH = 64;
-const int SURVIVOR_HEIGHT = 64;
+const int SURVIVOR_WIDTH = 48;
+const int SURVIVOR_HEIGHT = 48;
 
 /*VIEWPORTS*/
 //Info viewport
 const SDL_Color INFO_COLOR = { 47, 79, 79, 255 }; //dark slate gray
-const SDL_Rect INFO_VIEWPORT = { 10, 10, 940, 70 }; //x, y, width, height
-const SDL_Rect INFO = { 0, 0, 940, 70 };
+const SDL_Rect INFO_VIEWPORT = { 980, 10, 200, 576 }; //x, y, width, height
+const SDL_Rect INFO = { 0, 0, 200, 576 };
 
 //Game viewport
 const SDL_Color GAME_COLOR = { 105, 105, 105, 255 }; //dim gray
-const SDL_Rect GAME_VIEWPORT = { 10, 90, 940, 540 }; //x, y, width, height
-const SDL_Rect GAME = { 0, 0, 940, 540 };
-const int GAME_WIDTH = 940;
-const int GAME_HEIGHT = 540;
+const SDL_Rect GAME_VIEWPORT = { 10, 10, 960, 576 }; //x, y, width, height
+const SDL_Rect GAME = { 0, 0, 960, 576 };
+const int GAME_WIDTH = 960;
+const int GAME_HEIGHT = 576;
 
 //math constants
 const double PI = 3.141592653589793;
