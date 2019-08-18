8fbb8382f035834ae7ca4403e9d06ff8de8e6221
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-08-18 02:00:51 -0700

    Add direction field to Survivor class and some getter/setter methods
    Add direction field to Survivor class and some getter/setter methods to
    enable movement. Also, fix bug in load function of Survivor class

--- Survivor.cpp
+++ Survivor.cpp
@@ -1,16 +1,24 @@
 #include "Survivor.h"
+#include "Constants.h"
 
-Survivor::Survivor(int width, int height) {
+Survivor::Survivor(int x, int y, int width, int height) {
     texture = NULL;
+    this->x = x;
+    this->y = y;
     this->width = width;
     this->height = height;
+    this->direction = SURVIVOR_STARTING_DIRECTION;
 }
 Survivor::~Survivor() {
     delete texture;
 }
-void Survivor::draw(int x, int y, int anim_index) {
+void Survivor::draw(int anim_index) {
     texture->render(x, y, width, height, anim_index);
 }
 void Survivor::load(std::string path, int anim_rows, int anim_cols) {
+    if (texture != NULL) {
+        delete texture;
+    }
+    texture = new ObjTexture();
     texture->load(path, anim_rows, anim_cols);
 }
--- Survivor.h
+++ Survivor.h
@@ -5,20 +5,32 @@
 
 class Survivor {
     public:
-        Survivor(int width, int height);
+        Survivor(int x, int y, int width, int height);
         ~Survivor();
 
-        void draw(int x, int y, int anim_index);
+        void draw(int anim_index);
         void load(std::string path, int anim_rows, int anim_cols);
         void free();
 
         int getWidth() { return width; }
         int getHeight() { return height; }
+        int getX() { return x; }
+        int getY() { return y; }
+        int getDirection() { return direction; }
+        ObjTexture* getTexture() { return texture; }
+
+        void setX(int x) { this->x = x; }
+        void setY(int y) { this->y = y; }
+        void setDirection(int dir) { this->direction = dir; }
+
 
     private:
         ObjTexture* texture;
         int width;
         int height;
+        int x;
+        int y;
+        int direction; //0 = left, 1 = up, 2 = right, 3 = down
 };
 
 #endif
