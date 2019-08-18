0873af8f25c99d38903cc669b5b94c96e5fe452d
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-08-18 02:02:38 -0700

    Enable Player movement and clean up Survive.cpp code in the main function
    Use global variables instead of hardcoded values, clean up lots of extra
    unneeded code, and enable movement at the press of arrow keys

--- Survive.cpp
+++ Survive.cpp
@@ -1,11 +1,10 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
-#include "Constants.h"
 #include <string>
 
-//const int SCREEN_WIDTH = 640;
-//const int SCREEN_HEIGHT = 480;
+#include "Constants.h"
+#include "Survivor.h"
 
 //Initialize SDL and create window
 bool init();
@@ -13,13 +12,15 @@ bool init();
 //Shut down SDL
 void close();
 
-//Loads image as texture
-SDL_Texture* loadTexture( std::string path );
-
+//Globals
 SDL_Window* gWindow = NULL;
 SDL_Surface* gScreen = NULL;
 SDL_Renderer* gRenderer = NULL;
 SDL_Texture* gTexture = NULL;
+//Initialize Player
+Survivor survivor(GAME_WIDTH/2, GAME_HEIGHT/2, SURVIVOR_WIDTH, SURVIVOR_HEIGHT);
+//Initialize Enemies
+
 
 bool init() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -39,11 +40,12 @@ bool init() {
         printf("SDL_CreateRenderer: %s\n", SDL_GetError());
         return false;
     }
-    //Initialize renderer color
+    //Initialize renderer color to white for clearing screen
     SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+    SDL_RenderClear(gRenderer);
 
     //Initialize PNG loading
-    int flags = IMG_INIT_PNG;
+    int flags = IMG_INIT_PNG | IMG_INIT_JPG;
     int initted = IMG_Init(flags);
     if ((initted&flags) != flags) {
         printf("IMG_Init: %s\n", IMG_GetError());
@@ -52,32 +54,26 @@ bool init() {
 
     return true;
 }
-
-SDL_Texture* loadTexture( std::string path) {
-    //texture to return
+SDL_Texture* loadTexture(std::string path) {
     SDL_Texture* newTexture = NULL;
-
-    //load image
-    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-    if (loadedSurface == NULL) {
-        printf("IMG_Load: %s\n", IMG_GetError());
-    }
-    //Create texture from surface
-    newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
-    if (newTexture == NULL) {
-        printf("SDL_CreateTextureFromSurface: %s\n", SDL_GetError());
-    }
-    //Get rid of surface
-    SDL_FreeSurface(loadedSurface);
+    SDL_Surface* loaded = IMG_Load(path.c_str());
+    newTexture = SDL_CreateTextureFromSurface(gRenderer, loaded);
+    SDL_FreeSurface(loaded);
     return newTexture;
 }
+
 bool load() {
     bool success = true;
-    gTexture = loadTexture("person1.png");
-    if (gTexture == NULL) {
-        printf("Failed to load person1.png\n");
-        success = false;
-    }
+    //load main Character texture
+    survivor.load("Player.png", 4, 4);
+
+    //gTexture = survivor.getTexture()->getTexture();
+    //
+    //gTexture = loadTexture("mario.png");
+    //if (gTexture == NULL) {
+    //    printf("Failed to load person1.png\n");
+    //    success = false;
+    //}
     return success;
 }
 
@@ -107,6 +103,11 @@ int main( int argc, char* args[] ) {
 
     //Main loop flag
     bool quit = false;
+    int frame = 0;
+    int current = SDL_GetTicks();
+    int second_timer = current;
+    int last = current;
+    int frame_count = 0;
 
     //Event handler
     SDL_Event e;
@@ -118,53 +119,106 @@ int main( int argc, char* args[] ) {
                 quit = true;
             }
         }
+        const Uint8* currentKeyState = SDL_GetKeyboardState(NULL);
+        if (currentKeyState[SDL_SCANCODE_LEFT]) {
+            if (survivor.getDirection() != 0) {
+                frame = 0;
+                survivor.setDirection(0);
+            }
+        }
+        else if (currentKeyState[SDL_SCANCODE_UP]) {
+            if (survivor.getDirection() != 1) {
+                frame = 0;
+                survivor.setDirection(1);
+            }
+        }
+        else if (currentKeyState[SDL_SCANCODE_RIGHT]) {
+            if (survivor.getDirection() != 2) {
+                frame = 0;
+                survivor.setDirection(2);
+            }
+        }
+        else if (currentKeyState[SDL_SCANCODE_DOWN]) {
+            if (survivor.getDirection() != 3) {
+                frame = 0;
+                survivor.setDirection(3);
+            }
+        }
+        else {
+            if (survivor.getDirection() < 10) {
+                survivor.setDirection(survivor.getDirection()+10);
+            }
+        }
+
         //clear screen
-        SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+        SDL_SetRenderDrawColor( gRenderer, BG_COLOR.r, BG_COLOR.g, BG_COLOR.b, BG_COLOR.a );
         SDL_RenderClear(gRenderer);
-        //Set up viewport for in game info bar
-        SDL_Rect infoViewport = { 10, 10, 940, 50 };
-        SDL_RenderSetViewport(gRenderer, &infoViewport);
 
-        //render in game info bar
-        SDL_Rect boxPos;
-        boxPos.x = 0;
-        boxPos.y = 0;
-        boxPos.w = 940;
-        boxPos.h = 50;
+        //Set up viewport for info area
+        SDL_RenderSetViewport(gRenderer, &INFO_VIEWPORT);
 
-        SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
-        SDL_RenderDrawRect(gRenderer, &boxPos);
+        //Render info area
+        SDL_SetRenderDrawColor( gRenderer, INFO_COLOR.r, INFO_COLOR.g, INFO_COLOR.b, INFO_COLOR.a );
+        SDL_RenderFillRect(gRenderer, &INFO);
 
         //Set up viewport for gameplay area
-        SDL_Rect gameViewport = { 10, 70, 940, 560 };
-        SDL_RenderSetViewport(gRenderer, &gameViewport);
-        SDL_Rect box2Pos;
-        box2Pos.x = 0;
-        box2Pos.y = 0;
-        box2Pos.w = 940;
-        box2Pos.h = 560;
-        SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
-        SDL_RenderDrawRect(gRenderer, &box2Pos);
+        SDL_RenderSetViewport(gRenderer, &GAME_VIEWPORT);
 
-        SDL_Rect person1Pos;
-        person1Pos.x = 200;
-        person1Pos.y = 200;
-        person1Pos.w = 50;
-        person1Pos.h = 50;
-        SDL_Rect animation1;
-        animation1.x = 0;
-        animation1.y = 0;
-        animation1.w = 64;
-        animation1.h = 64;
+        //Render gameplay area
+        SDL_SetRenderDrawColor(gRenderer, GAME_COLOR.r, GAME_COLOR.g, GAME_COLOR.b, GAME_COLOR.a);
+        SDL_RenderFillRect(gRenderer, &GAME);
 
         //render gameplay area
         //render texture to the renderer to render to screen
-        SDL_RenderCopy(gRenderer, gTexture, &animation1, &person1Pos);
-        //SDL_RenderCopy(gRenderer, gTexture, &animation2, &person2Pos);
-        //SDL_RenderCopy(gRenderer, gTexture, &animation3, &person3Pos);
-        //SDL_RenderCopy(gRenderer, gTexture, &animation4, &person4Pos);
+        if (survivor.getDirection() == 0) {
+            survivor.draw(frame/6);
+            survivor.setX(survivor.getX()-2);
+        }
+        else if (survivor.getDirection() == 1) {
+            survivor.draw(4 + frame/6);
+            survivor.setY(survivor.getY()-2);
+        }
+        else if (survivor.getDirection() == 2) {
+            survivor.draw(8 + frame/6);
+            survivor.setX(survivor.getX()+2);
+        }
+        else if (survivor.getDirection() == 3) {
+            survivor.draw(12 + frame/6);
+            survivor.setY(survivor.getY()+2);
+        }
+        else if (survivor.getDirection() == 10) {
+            survivor.draw(1);
+        }
+        else if (survivor.getDirection() == 11) {
+            survivor.draw(5);
+        }
+        else if (survivor.getDirection() == 12) {
+            survivor.draw(9);
+        }
+        else if (survivor.getDirection() == 13) {
+            survivor.draw(13);
+        }
+        //survivor.draw(frame/16);
         //update screen
         SDL_RenderPresent(gRenderer);
+
+        frame = (frame + 1) % 24;
+
+        //end frame FrameManager checks
+        frame_count++;
+        current = SDL_GetTicks();
+        //capture frame rate
+        if (current - second_timer >= 1000) {
+            printf("Frame Rate = %dfps\n", frame_count);
+            second_timer = current;
+            frame_count = 0;
+        }
+        //cap frame rate
+        int since_last = current - last;
+        if ( (current - last) < MS_PER_FRAME ) {
+            SDL_Delay( MS_PER_FRAME - (current-last) );
+        }
+        last = SDL_GetTicks();
 
     }
 
