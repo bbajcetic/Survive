19c65e4ea5f6aaf30b2f3ff51d4984b6360bbf4e
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-08-27 18:58:33 -0700

    Add Collision class for collision detection
    Add function for detecting collision of rotating rectangular hitboxes

---
+++ Collision.cpp
@@ -0,0 +1,39 @@
+#include "Collision.h"
+
+bool detectCollision(Vec2D A[], Vec2D B[]) {
+    /* Parameters A and B are Vec2D arrays of size 4, defining a rectangle by
+     * its corners, in a clockwise or counter-clockwise manner */
+
+    //get 4 axes
+    Vec2D axes[4] = { {A[1]-A[0]}, {A[2]-A[1]}, {B[1]-B[0]}, {B[2]-B[1]} };
+    for (int i = 0; i < 4; i++) {
+        /* current axis = axes[i] */
+        Vec2D projs[8];
+        for (int j = 0; j < 4; ++j) {
+            projs[j] = vecProject(A[j], axes[i]);
+            projs[j+4] = vecProject(B[j], axes[i]);
+        }
+        //find min and max projected vectors for each rect
+        float dots[8];
+        for (int j = 0; j < 8; ++j) {
+            dots[j] = projs[j] * axes[i];
+            //printf("dot %d = %f\n", j, dots[j]);
+        }
+        float minA, maxA, minB, maxB;
+        minA = maxA = dots[0];
+        minB = maxB = dots[4];
+        for (int j = 0; j < 4; ++j) {
+            if (dots[j] < minA) { minA = dots[j]; }
+            else if (dots[j] > maxA) { maxA = dots[j]; }
+            if (dots[j+4] < minB) { minB = dots[j+4]; }
+            else if (dots[j+4] > maxB) { maxB = dots[j+4]; }
+        }
+        if (minB <= maxA && maxB >= minA) {
+            continue;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ Collision.h
@@ -0,0 +1,3 @@
+#include "Vec2D.h"
+
+bool detectCollision(Vec2D A[], Vec2D B[]);
